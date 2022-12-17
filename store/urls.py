72e41c9031a2eb55ad6bcd093dda48599e7c56c9
coded <lilcoded7@example.com>
2022-12-17 08:20:50 -0800

    DJANGO ECOMMERCE

---
+++ store/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+
+
+urlpatterns = [
+    path('', views.store, name='home'),
+    path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('update-item/', views.updateItem, name='update-item')
+   
+]
