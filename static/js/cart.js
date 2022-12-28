ea6f7d47f561d089eecc5a6bcc9fad6ba020b7a6
coded <lilcoded7@example.com>
2022-12-28 07:25:50 -0800

    updated

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,8 +1,5 @@
 let updateBtns = document.getElementsByClassName('update-cart') 
 
-
-
-
 for(let i = 0; i < updateBtns.length; i++){
     updateBtns[i].addEventListener('click', function(){
         let productId = this.dataset.product 
@@ -12,13 +9,41 @@ for(let i = 0; i < updateBtns.length; i++){
         console.log('User', user)
 
         if (user === 'AnonymousUser'){
-            console.log('Anonymous User, Not logged in')
+            addCookieItem(productId, action)
         }else{
             updateUserOrder(productId, action)
         }
 
     })
 }
+
+function addCookieItem(productId, action){
+    console.log('User is Not logged in...')
+    
+    if(action == 'add'){
+        if(cart[productId] == undefined){
+            cart[productId] = {'quantity':1}
+            console.log('Cart productid is undefine...')
+        }else{
+            cart[productId]['quantity'] += 1
+            console.log('Cart Updated...')
+        }
+    }
+
+    if(action == 'remove'){
+        cart[productId]['quantity'] -= 1
+
+        if(cart[productId]['quantity'] <= 0){
+            console.log('Item Deleted')
+            delete cart[productId]
+        }
+    }
+    console.log('Cart:', cart)
+    document.cookie = 'cart=' + JSON.stringify(cart) + ';domain=;path=/'
+    location.reload()
+}
+
+
 
 function updateUserOrder(productId, action){
     console.log('User is logged in Sending data...')
@@ -48,7 +73,7 @@ function updateUserOrder(productId, action){
    
 
 
-// function addCookieItem(productId, action){
+// {
 //     console.log('Not logged in..')
 //     if(action == 'add'){
 //         if(cart[productId] == undefined){
@@ -76,13 +101,4 @@ function updateUserOrder(productId, action){
 
  
 
-// {
-//     console.log('User is logged in, sending data...')
-
-//     let url = '/update-item/'
-
-//     
-    
-
-//     
 
