a3220669a4acd48e593c6980f77118444e14d973
coded <coded@example.com>
2023-02-17 23:52:25 -0800

    updated

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -81,23 +81,23 @@ CSRF_TRUSTED_ORIGINS = ['https://ecommerce-production-dd17.up.railway.app/']
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE'   : 'django.db.backends.postgresql',
-        'NAME'     : 'railway',
-        'USER'     : 'postgres',
-        'PASSWORD' : 'rBchi8GfKNDxRd2loUmZ',
-        'HOST'     : 'containers-us-west-195.railway.app',
-        'PORT'     : '6905',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE'   : 'django.db.backends.postgresql',
+#         'NAME'     : 'railway',
+#         'USER'     : 'postgres',
+#         'PASSWORD' : 'rBchi8GfKNDxRd2loUmZ',
+#         'HOST'     : 'containers-us-west-195.railway.app',
+#         'PORT'     : '6905',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
